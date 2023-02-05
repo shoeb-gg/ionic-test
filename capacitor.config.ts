52d4511664668441565e6fb9febce34f84b4b4e0
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-02-06 00:48:37 +0600

    added android live reload and modifying UI

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Calculator',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://192.168.68.1:6200',
+    cleartext: true,
+  },
 };
 
 export default config;
